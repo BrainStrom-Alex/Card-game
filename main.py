fee0ca82e4910fc28ce07eb58e4e2f6a6f5063f9
Ravish Kumar Verma <brainstrom.alex@gmail.com>
2026-07-15 16:41:03 +0530

    add play game function

--- main.py
+++ main.py
@@ -34,3 +34,30 @@ def compare(u_score, c_score):
     else:
         return "You lose"
 
+def play_game():
+    print(art.logo)
+    user_card = []
+    computer_card = []
+    computer_score = -1
+    user_score = -1
+
+    is_game_over = False
+
+    for _ in range(2):
+        user_card.append(deal_card())
+        computer_card.append(deal_card())
+
+    while not is_game_over:
+        user_score = calculater_score(user_card)
+        computer_score = calculater_score(computer_card)
+        print(f"Your cards: {user_card}, current score: {user_score}")
+        print(f"Computer's first card: {computer_card[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input("Type 'y' to get another card or type 'n' to pass: ")
+            if user_should_deal == "y":
+                user_card.append(deal_card())
+            else:
+                is_game_over = True
