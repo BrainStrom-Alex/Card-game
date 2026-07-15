72e948d968a83e0e86a346ec115dac29ca6443f8
Ravish Kumar Verma <brainstrom.alex@gmail.com>
2026-07-15 16:39:21 +0530

    add calculater score

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
+
+
+def calculater_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
